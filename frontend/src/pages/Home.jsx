b8b1af4a7da95aa9c4565f215e0d931bbaa48e4d
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-23 09:29:37 +0530

    Create Home Page Global Event Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import News from '../components/News';
 import Slides from '../components/Slides';
 import Discover from '../components/Discover';
+import earthday from '../assets/earthday.jpg';
 
 export default function Home() {
   return (
@@ -9,6 +11,11 @@ export default function Home() {
     <div>
       <Slides />
       <News />
+      <div className='rounded-lg p-20'>
+          <h1 className='text-2xl sm:text-4xl font-serif text-center mb-5'>Global Events</h1>
+          <hr className='my-1 sm:my-2 border-2 border-gray-500 font-bold' />
+          <img src={earthday} alt="card-image" className='w-full h-auto rounded-3xl mt-10' />
+      </div>
       <Discover />
     </div>
   );
