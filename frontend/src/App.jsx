31cdba0ecb02f31bef4510c007e1baa3801fe255
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-23 08:10:39 +0530

    Create Pages And Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Astronomy from './pages/Astronomy';
+import Mars from './pages/Mars';
+import More from './pages/More';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={ <Home/> }/>
+        <Route path="/astronomy" element={ <Astronomy/> }/>
+        <Route path="/mars" element={ <Mars/> }/>
+        <Route path="/more" element={ <More/> }/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ frontend/src/pages/Astronomy.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Astronomy() {
+  return (
+    <div>Astronomy</div>
+  )
+}
