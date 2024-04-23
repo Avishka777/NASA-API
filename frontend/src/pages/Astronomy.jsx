ac851f13847f95ddb16603aeaabdfac5d1fbd6c2
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-23 09:22:05 +0530

    Create Mars Rover Page

--- frontend/src/pages/Astronomy.jsx
+++ frontend/src/pages/Astronomy.jsx
@@ -39,6 +39,7 @@ export default function Astronomy() {
 
   function getTodayDateString() {
     const today = new Date();
+    today.setDate(today.getDate() - 1);
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0');
     const day = String(today.getDate()).padStart(2, '0');
--- frontend/src/pages/Mars.jsx
+++ frontend/src/pages/Mars.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Label, Button } from 'flowbite-react';
 
 export default function Mars() {
+  const [photos, setPhotos] = useState([]);
+  const [rover, setRover] = useState('curiosity'); // Default rover is Curiosity
+  const [camera, setCamera] = useState(''); // Default camera is empty (no filter)
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetchPhotos(rover, camera);
+  }, [rover, camera]); // Fetch photos whenever the rover or camera changes
+
+  const fetchPhotos = async (rover, camera) => {
+    setError('');
+    try {
+      let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=tMfdGz37Ta0SE7RrlyoWNyM0HFQ5uDbNpa7BFL9d&date`;
+      if (camera) {
+        url += `&camera=${camera}`;
+      }
+      const res = await fetch(url);
+      const data = await res.json();
+      setPhotos(data.latest_photos);
+      console.log(data.latest_photos);
+    } catch (error) {
+      setError('Error fetching photos from NASA API');
+      console.error(error);
+    }
+  };
+
+  const handleRoverChange = (event) => {
+    setRover(event.target.value);
+  };
+
+  const handleCameraChange = (event) => {
+    setCamera(event.target.value);
+  };
+
   return (
-    <div>Mars</div>
-  )
+    <div className="p-10 ">
+      <form>
+        <div className="flex flex-col md:flex-row items-center gap-4 mt-3 md:mt-0">
+          <Label htmlFor="roverSelect" className="text-sky-600 font-bold text-xl">
+            Select Rover :
+          </Label>
+          <select
+            id="roverSelect"
+            value={rover}
+            onChange={handleRoverChange}
+            className="px-2 py-1 border border-gray-300 rounded-md w-full md:w-60 h-10"
+          >
+            <option value="curiosity">Curiosity</option>
+            <option value="opportunity">Opportunity</option>
+            <option value="spirit">Spirit</option>
+          </select>
+          <Label htmlFor="cameraSelect" className="text-sky-600 font-bold text-xl">
+            Select Camera :
+          </Label>
+          <select
+            id="cameraSelect"
+            value={camera}
+            onChange={handleCameraChange}
+            className="px-2 py-1 border border-gray-300 rounded-md w-full md:w-60 h-10"
+          >
+            <option value="">All Cameras</option>
+            <option value="fhaz">Front Hazard Avoidance Camera</option>
+            <option value="rhaz">Rear Hazard Avoidance Camera</option>
+            <option value="mahli">Mars Hand Lens Imager</option>
+            <option value="navcam">Navigation Camera</option>
+          </select>
+          <Button gradientDuoTone="purpleToBlue" type="button" onClick={() => fetchPhotos(rover, camera)}>
+            Search
+          </Button>
+        </div>
+      </form>
+
+      {error && <div>{error}</div>}
+      <div className="mt-10">
+        {photos.map((photo) => (
+          <div key={photo.id} className="mb-8">
+            <div className='flex flex-col md:flex-row'>
+              <img src={photo.img_src} alt={photo.id} className="max-w-full md:max-w-screen-sm md:max-h-screen rounded-3xl mb-6 md:mb-0" />
+              <div className='md:ml-10'>
+                <span className='text-3xl font-semibold'>Rover Details</span>
+                <hr className='my-2 border-2 border-blue-500 font-bold' />
+                <p className="text-lg font-semibold text-zinc-700">Rover ID : {photo.camera.rover_id}</p>
+                <p className="text-lg font-semibold text-zinc-700">Rover Name : {photo.rover.name}</p>
+                <p className="text-lg font-semibold text-zinc-700">Rover Launch Date : {photo.rover.launch_date}</p>
+                <p className="text-lg font-semibold text-zinc-700">Rover Landing Date : {photo.rover.landing_date}</p>
+                <p className="text-lg font-semibold text-zinc-700 mb-5">Rover Status: {photo.rover.status}</p>
+                <span className='text-3xl font-semibold'>Camera Details</span>
+                <hr className='my-2 border-2 border-blue-500 font-bold' />
+                <p className="text-lg font-semibold text-zinc-700">Camera Short Name : {photo.camera.name}</p>
+                <p className="text-lg font-semibold text-zinc-700 mb-5">Camera Full Name : {photo.camera.full_name}</p>
+                <span className='text-3xl font-semibold'>Other Details</span>
+                <hr className='my-2 border-2 border-blue-500 font-bold' />
+                <p className="text-lg font-semibold text-zinc-700">Earth Date: {photo.earth_date}</p>
+                <p className=' text-lg font-serif text-sky-700'>View HD Image : <a href={photo.img_src} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">{photo.img_src}</a></p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
