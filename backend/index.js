f24ac689011c736e3b06f6281c20ced0bb7fbd23
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-25 11:53:03 +0530

    Create User Model And Test API Route

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,34 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+//import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 import path from 'path';
 
 dotenv.config();
-const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
 
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
 app.listen(3000, () => {
-  console.log('Server Is Running On Port 3000 !');
+  console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user', userRoutes);
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
@@ -18,12 +36,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('MongoDB Is Connected');
-  })
-  .catch((err) => {
-    console.log(err);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
-
+});
---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required:true,
+    },
+    }, {timestamps: true}
+
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
