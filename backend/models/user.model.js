b5f71f958dbd73469affac5263cf2df4c1112cb6
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-27 06:52:22 +0530

    Update Header And Footer Component With User Data

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -28,6 +28,11 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    profilePicture: {
+        type: String,
+        default:
+          'https://firebasestorage.googleapis.com/v0/b/mern-university-management.appspot.com/o/profile_photos%2Fuser.JPG?alt=media&token=10403a5e-61fa-4c1c-a5a9-7b2560ba3b05',
+    },
     }, {timestamps: true}
 
 );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,67 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 
 export default function Header() {
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
+    <Navbar className='border-b-2'>
 
-    <Navbar className='shadow-lg'>
-      <Navbar.Toggle className='ml-3' />
-
-      <Navbar.Brand className='ml-20'>
+      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white flex'>
         <img src={logo} className="mr-3 h-16 sm:h-16" alt="Company Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">NASA API</span>
-      </Navbar.Brand>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white font-serif">NASA API</span>
+      </Link>
 
-      <div className="flex md:order-2 mr-20">
-        <Button className='w-14 h-10 hidden sm:inline mr-5' color='gray' pill>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-14 h-12 mr-4  sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
-        </Link> 
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='pinkToOrange' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle />
       </div>
-
       <Navbar.Collapse>
-        <Navbar.Link href="/">HOME</Navbar.Link>
-        <Navbar.Link href="/astronomy">ASTRONOMY PICTURES</Navbar.Link>
-        <Navbar.Link href="/mars">MARS ROVER</Navbar.Link>
-        <Navbar.Link href="/more">MORE</Navbar.Link>
+        <Navbar.Link active={path === '/'} as={'div'} style={{ color: path === "/" ? "#C12c2e" : "#2c4ac1" }} className='text-lg'>
+          <Link to='/'>HOME</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/astronomy'} as={'div'} style={{ color: path === "/astronomy" ? "#C12c2e" : "#2c4ac1" }} className='text-lg'>
+          <Link to='/astronomy'>ASTRONOMY</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/mars'} as={'div'} style={{ color: path === "/mars" ? "#C12c2e" : "#2c4ac1" }} className='text-lg'>
+          <Link to='/mars'>MARS</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/more'} as={'div'} style={{ color: path === "/more" ? "#C12c2e" : "#2c4ac1" }} className='text-lg' >
+          <Link to='/more'>MORE</Link>
+        </Navbar.Link>
       </Navbar.Collapse>
-
     </Navbar>
   );
 }
