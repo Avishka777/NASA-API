24025b9ed46c1d7cac1bbe683ba160f2c3a8a50a
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-27 06:50:52 +0530

    Add Redux Persist

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -42,7 +42,7 @@ export default function SignUp() {
     }
   };
   return (
-    <div className='min-h-screen mt-10'>
+    <div className='min-h-screen'>
       <div className='flex p-3 max-w-7xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
         <div className='flex-1 flex flex-col items-center justify-center mx-8'>
