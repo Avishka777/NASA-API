2aa0a1586b195fab854cd71962f4fd7f59fbbedc
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-27 06:56:38 +0530

    Add Sign Out Function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,15 @@
 export const test = (req, res) => {
     res.json({ message: 'API is working!' });
 };
+
+//Sign Out User
+export const signout = (req, res, next) => {
+    try {
+      res
+        .clearCookie('access_token')
+        .status(200)
+        .json('User Has Been Signed Out');
+    } catch (error) {
+      next(error);
+    }
+  };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,33 @@
-import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleTheme } from '../redux/theme/themeSlice';
+import { Link, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Avatar, Button, Dropdown, Navbar } from 'flowbite-react';
 import logo from '../assets/logo.png';
+import { toggleTheme } from '../redux/theme/themeSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
 
 export default function Header() {
   const path = useLocation().pathname;
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <Navbar className='border-b-2'>
@@ -18,9 +36,9 @@ export default function Header() {
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white font-serif">NASA API</span>
       </Link>
 
-      <div className='flex gap-2 md:order-2'>
+      <div className='flex gap-2 md:order-2 my-auto'>
         <Button
-          className='w-14 h-12  mr-4 sm:inline rounded-lg'
+          className='w-14 h-11  mr-4 sm:inline rounded-lg'
           color='gray'
           pill
           onClick={() => dispatch(toggleTheme())}
@@ -36,35 +54,32 @@ export default function Header() {
             }
           >
             <Dropdown.Header>
-              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm'>@{currentUser.userName}</span>
               <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
             </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
-              <Dropdown.Item>Profile</Dropdown.Item>
-            </Link>
-            <Dropdown.Divider />
-            <Dropdown.Item>Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to='/sign-in'>
-            <Button gradientDuoTone='pinkToOrange' outline>
+            <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
           </Link>
         )}
         <Navbar.Toggle />
       </div>
+
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'} style={{ color: path === "/" ? "#C12c2e" : "#2c4ac1" }} className='text-lg'>
+        <Navbar.Link active={path === '/'} as={'div'} style={{ color: path === "/" ? "#C71014" : "#807e87" }} className='text-lg'>
           <Link to='/'>HOME</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/astronomy'} as={'div'} style={{ color: path === "/astronomy" ? "#C12c2e" : "#2c4ac1" }} className='text-lg'>
+        <Navbar.Link active={path === '/astronomy'} as={'div'} style={{ color: path === "/astronomy" ? "#C71014" : "#807e87" }} className='text-lg'>
           <Link to='/astronomy'>ASTRONOMY</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/mars'} as={'div'} style={{ color: path === "/mars" ? "#C12c2e" : "#2c4ac1" }} className='text-lg'>
+        <Navbar.Link active={path === '/mars'} as={'div'} style={{ color: path === "/mars" ? "#C71014" : "#807e87" }} className='text-lg'>
           <Link to='/mars'>MARS</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/more'} as={'div'} style={{ color: path === "/more" ? "#C12c2e" : "#2c4ac1" }} className='text-lg' >
+        <Navbar.Link active={path === '/more'} as={'div'} style={{ color: path === "/more" ? "#C71014" : "#807e87" }} className='text-lg' >
           <Link to='/more'>MORE</Link>
         </Navbar.Link>
       </Navbar.Collapse>
